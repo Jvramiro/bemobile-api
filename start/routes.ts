494b74eed87b81a72fd9ea2b9127c4ee8377d904
João Victor Ramiro <85127729+Jvramiro@users.noreply.github.com>
2026-03-13 13:05:25 -0300

    Update Routes

--- start/routes.ts
+++ start/routes.ts
@@ -1,37 +1,48 @@
-/*
-|--------------------------------------------------------------------------
-| Routes file
-|--------------------------------------------------------------------------
-|
-| The routes file is used for defining the HTTP routes.
-|
-*/
-
-import { middleware } from '#start/kernel'
 import router from '@adonisjs/core/services/router'
-import { controllers } from '#generated/controllers'
+import { middleware } from './kernel.ts';
 
-router.get('/', () => {
-  return { hello: 'world' }
-})
+import AccessTokenController from "#controllers/access_token_controller";
+import NewAccountController from '#controllers/new_account_controller';
+import PurchasesController from '#controllers/purchases_controller';
+import UsersController from '#controllers/users_controller';
+import ProductsController from '#controllers/products_controller';
+import ClientsController from '#controllers/clients_controller';
+import TransactionsController from '#controllers/transactions_controller';
+import GatewaysController from '#controllers/gateways_controller';
 
-router
-  .group(() => {
-    router
-      .group(() => {
-        router.post('signup', [controllers.NewAccount, 'store'])
-        router.post('login', [controllers.AccessToken, 'store'])
-        router.post('logout', [controllers.AccessToken, 'destroy']).use(middleware.auth())
-      })
-      .prefix('auth')
-      .as('auth')
+//Public
+router.post('/login', [AccessTokenController, 'store'])
+router.post('/register', [NewAccountController, 'store'])
+router.post('/purchase', [PurchasesController, 'store'])
 
-    router
-      .group(() => {
-        router.get('/profile', [controllers.Profile, 'show'])
-      })
-      .prefix('account')
-      .as('profile')
-      .use(middleware.auth())
-  })
-  .prefix('/api/v1')
+//Private
+router.group(() =>{
+
+  //Users
+  router.get('/users', [UsersController, 'index'])
+  router.post('/users', [UsersController, 'store'])
+  router.get('/users/:id', [UsersController, 'show'])
+  router.put('/users/:id', [UsersController, 'update'])
+  router.delete('/users/:id', [UsersController, 'destroy'])
+
+  // Produtos
+  router.get('/products', [ProductsController, 'index'])
+  router.post('/products', [ProductsController, 'store'])
+  router.get('/products/:id', [ProductsController, 'show'])
+  router.put('/products/:id', [ProductsController, 'update'])
+  router.delete('/products/:id', [ProductsController, 'destroy'])
+
+  // Clientes
+  router.get('/clients', [ClientsController, 'index'])
+  router.get('/clients/:id', [ClientsController, 'show'])
+
+  // Transações
+  router.get('/transactions', [TransactionsController, 'index'])
+  router.get('/transactions/:id', [TransactionsController, 'show'])
+  router.post('/transactions/:id/refund', [TransactionsController, 'refund'])
+
+  // Gateways
+  router.patch('/gateways/:id/active', [GatewaysController, 'setActive'])
+  router.patch('/gateways/:id/priority', [GatewaysController, 'updatePriority'])
+
+}).use(middleware.auth())
